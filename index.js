8b690935b266fd3c7761c56b99399fe5a3d193cb
kisanbhat <kisan.bhat@gmail.com>
2016-06-14 09:43:34 +0530

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-var uniqueRandaomArray= require('unique-random-array');
-var starWarsNames =require('./starwars-names.json');
-module.exports ={
-	all:starWarsNames,
-	random:uniqueRandaomArray(starWarsNames)
-};
